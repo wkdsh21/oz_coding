c6013d027eb9beb5eb444d5d451e65ad6c62aeda
wkdsh21 <wkdsh21@naver.com>
2024-08-30 16:49:56 +0900

    js 과제1.설문조사페이지

---
+++ js/1회차/js1회차.js
@@ -0,0 +1,37 @@
+window.alert("ㅎㅇ")
+alert("dd")
+console.log(10)
+console.log(10.1)
+console.log(10*2)
+console.log(10/2)
+console.log(10%3)
+console.log(typeof 2)
+console.log(typeof 2.2)
+
+const num=2
+// num=3
+
+let apple ="dd"
+let b="wwww"
+console.log(apple + b)
+
+let answer = prompt("당신이 좋아하는 숫자는?")
+alert(answer)
+
+const pretty="귀여운"
+
+const pp=`${pretty} ${prompt("좋아하는 동물은?")}`
+alert(pp)
+
+let name =undefined
+console.log(typeof name)
+
+name= null
+console.log(typeof name)
+
+let answer2= confirm("기분이어떠세요")
+alert(answer) 
+
+const p = document.querySelector("p")
+console.log(p.textContent)
+p.textContent="changed"
