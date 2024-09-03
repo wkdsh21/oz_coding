adcf0430c0f87bf36b28a10077eadd68dc205bac
wkdsh21 <wkdsh21@naver.com>
2024-09-03 15:43:31 +0900

    어드민 페이지

---
+++ admin_page/database.js
@@ -0,0 +1,62 @@
+//데이터베이스
+const data = [
+    { category: "상의", brand: 'Supreme', product: '슈프림 박스로고 후드티', price: '390,000' },
+    { category: "하의", brand: 'DIESEL', product: '디젤 트랙 팬츠', price: '188,000' },
+    { category: "신발", brand: 'Nike', product: '에어포스 1', price: '137,000' },
+    { category: "패션잡화", brand: 'Music&Goods', product: '빵빵이 키링', price: '29,000' },
+];
+const dataTable = document.getElementById('data-table');
+// 초기화
+data.forEach((item) => {
+    const row = dataTable.insertRow();
+    row.insertCell(0).innerHTML = item.category;
+    row.insertCell(1).innerHTML = item.brand;
+    row.insertCell(2).innerHTML = item.product;
+    row.insertCell(3).innerHTML = item.price;
+});
+
+//카테고리 정렬 
+let category=document.querySelector("#inlineFormSelectPref")
+category.addEventListener('change', function() {
+    const selectedValue = category.value;
+    const dataTable = document.getElementById('data-table');
+    //모두 제거후
+    let count=dataTable.rows.length
+    while(0<count){
+        dataTable.deleteRow(count-1)
+        count-=1
+    }
+    // 다시생성
+    data.forEach((item) => {
+        if(item.category==selectedValue || selectedValue=="전체"){
+            console.log("dd")
+            const row = dataTable.insertRow();
+            row.insertCell(0).innerHTML = item.category;
+            row.insertCell(1).innerHTML = item.brand;
+            row.insertCell(2).innerHTML = item.product;
+            row.insertCell(3).innerHTML = item.price;
+        }
+    });
+});
+
+
+//시계
+setInterval(() =>{
+    let today = new Date();  
+    let clock = document.querySelector("#clock")
+    clock.textContent=`${today.getFullYear()}/${today.getMonth()}/${today.getDate()} ${today.getHours()} : ${today.getHours()} : ${today.getSeconds()}`
+},100)
+
+//다크모드 설정
+let btn = document.querySelector("#flexSwitchCheckDefault")
+let btnLabel = document.querySelector(".form-check-label")
+btn.addEventListener("click",function(event){
+    if(btn.checked){
+        document.querySelector("#product").classList.add('dark-mode');
+        btnLabel.textContent="다크모드 ON"
+    }
+    else{
+        document.querySelector("#product").classList.remove('dark-mode');
+        btnLabel.textContent="다크모드 OFF"
+    }
+})
