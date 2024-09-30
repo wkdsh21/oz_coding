9f214ef4c5ecc9196e75568cc9229b95b6d1f9b3
wkdsh21 <wkdsh21@naver.com>
2024-09-30 18:18:10 +0900

    db 과제1

--- python/14.py
+++ python/14.py
@@ -2,3 +2,19 @@
 # 람다 == 익명함수
 # filter(lambda x : x < 5 , list)
 # 람다는 가독성,편리함,간단함 map,filter 같은곳에 콜백 함수로써 선언과 동시에 사용가능
+from typing import Callable, Iterable, Any
+
+a = list(range(1, 11))
+a = filter(lambda x: x % 2 == 0, a)
+print(list(a))
+
+
+def my_filter(func: Callable[[Any], bool], iterable: Iterable) -> list:
+    result = []
+    for i in iterable:
+        if func(i):
+            result.append(i)
+    return result
+
+
+my_filter(lambda x: x % 2 == 0, list(range(1, 11)))
--- python/16.py
+++ python/16.py
@@ -1,5 +1,8 @@
 from animals.mammals import Dog
 from animals.birds import Eagle
 
-print(Eagle("bb", 4))
-print(Dog("aa", 3))
+# print(Eagle("bb", 4))
+# print(Dog("aa", 3))
+a = {"1": "@", "2": "2"}
+for i in a:
+    print(i)
